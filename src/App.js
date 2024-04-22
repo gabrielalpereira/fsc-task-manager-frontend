362e17a185e425eaf8836240b37f6dc9df42a204
Gabriela Pereira <gabrielalara3010@gmail.com>
2024-04-22 15:35:35 -0300

    refactor: convert component to class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import { useState } from "react";
+import React from "react";
 import TaskItem from "./components/TaskItem";
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleStateChange = this.handleStateChange.bind(this);
+        this.state = {
+            tasks: [
+                {
+                    id: "1",
+                    description: "Estudar programação",
+                    isCompleted: false,
+                },
+                {
+                    id: "2",
+                    description: "Ler",
+                    isCompleted: true,
+                },
+            ],
+        };
+    }
 
-const App = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: "1",
-            description: "Estudar programação",
-            isCompleted: false,
-        },
-        {
-            id: "2",
-            description: "Ler",
-            isCompleted: true,
-        },
-    ]);
+    handleStateChange() {
+        this.setState({
+            tasks: [],
+        });
+    }
 
-    return (
-        <>
-            {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
-            ))}
-        </>
-    );
-};
+    render() {
+        return (
+            <>
+                {this.state.tasks.map((task) => (
+                    <TaskItem key={task.id} task={task} />
+                ))}
+                <button onClick={this.handleStateChange}>Limpar Tarefas</button>
+            </>
+        );
+    }
+}
 
 export default App;
